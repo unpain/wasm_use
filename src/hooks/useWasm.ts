36b56d8ee5c1b196bb596f51b143ebe437745046
fangshuo <1733636949@qq.com>
2024-02-29 18:00:29 +0800

    feat:upd

--- src/hooks/useWasm.ts
+++ src/hooks/useWasm.ts
@@ -3,7 +3,6 @@ const useWasm = (url: string, target: Ref) => {
   if (!target.value) {
     WebAssembly.instantiateStreaming(fetch(url)).then((obj: WebAssembly.WebAssemblyInstantiatedSource) => {
       target.value = obj.instance.exports
-      console.log(target.value)
     })
   }
 }
